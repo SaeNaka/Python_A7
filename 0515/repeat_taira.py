3c4e3197037aaff9cf885c46d3c34f4d57d02b9b
Tairasato <sato.taira.cs@mynavi.jp>
2024-05-15 02:53:39 +0000

    repeat_taira & sleep_taira

---
+++ 0515/repeat_taira.py
@@ -0,0 +1,14 @@
+import sys
+args = sys.argv
+sum = 0
+a = int(args[1])
+
+while int(sum)<100:
+    sum = sum+a
+
+else:
+    if sum == 100:
+        print("Just 100!")
+    else :
+        print("Over!")
+          
---
+++ 0515/sleep_taira.py
@@ -0,0 +1,10 @@
+import sys
+args = sys.argv
+a = int(args[1])
+
+hituji =0
+for i in range(1,a+1):
+    hituji = hituji +1
+    print (f"ひつじが{hituji}匹")
+
+ 
