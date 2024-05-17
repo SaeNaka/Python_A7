767456bf1354ec320d33f72d946a8bccf6d7aeca
RihoYajima <yajima.riho.mn@mynavi.jp>
2024-05-17 08:00:26 +0000

    "修正版：素因数分解"

--- Challenge/primefiactor_yajima.py
+++ Challenge/primefiactor_yajima.py
@@ -8,7 +8,7 @@ target = num
 results = []
 
 for i in range(2, num+1):
-    # i(=2,3,・・・num-1)で割り切れたらリストに追加
+    # i(=2,3,・・・num)で割り切れたらリストに追加
     while target % i == 0:
         results.append(i)
         target /= i
