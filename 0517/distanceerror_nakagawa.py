dfa1bba299ba58b234002b35a16c5d6531002a6e
SaeNaka <nakagawa.sae.lx@mynavi.jp>
2024-05-17 05:41:18 +0000

    add：error

---
+++ 0517/distanceerror_nakagawa.py
@@ -0,0 +1,21 @@
+import sys
+args = sys.argv
+#引数を変数に代入
+station_start = args[1]
+station_last = args[2]
+
+#辞書を作成
+distances = {
+    '東京': 0.00, '品川': 6.78,  '新横浜': 25.54, '名古屋': 342.02, '京都': 476.31, '新大阪': 515.35
+}
+
+try:
+    #引数間の距離の計算
+    distance = abs(distances[station_last] - distances[station_start])
+    #小数点第二位で四捨五入
+    distance = round(distance, 2)
+
+    print(distance, end = '')
+
+except KeyError:
+    print("のぞみの停車駅を引数に設定してください", end= '')
