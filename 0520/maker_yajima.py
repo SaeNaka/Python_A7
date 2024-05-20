48d00f5a291d5a8088611862ed05af79b1de2ca0
RihoYajima <yajima.riho.mn@mynavi.jp>
2024-05-20 01:06:53 +0000

    "修正：QRコードの出力"

--- 0520/maker_yajima.py
+++ 0520/maker_yajima.py
@@ -1,18 +1,20 @@
 # パッケージをインポート
 import qrcode
 import sys
+import os
 
 args = sys.argv
 qr_url = args[1] # QRコードを作成するURL
-file_path = args[2] # QRコードのPNGファイルのパス
+file_name = args[2] # QRコードのPNGファイル名
 
 
 # QRコードを生成
 img = qrcode.make(qr_url)
 
 # ファイルに保存
-img.save(file_path)
+path = os.path.join("./", "files", file_name)
+img.save(path)
 
 
 # 実行例
-# python ./0520/maker_yajima.py https://job.mynavi.jp/2025/ ./files/mynavi_qr_yajima.png
+# python ./0520/maker_yajima.py https://job.mynavi.jp/2025/ mynavi_qr_yajima.png
